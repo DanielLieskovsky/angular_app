eda1d180148247078388d36a54f88683f8c3be3f
Daniel Lieskovsky <danol147@gmail.com>
2022-07-10 21:17:53 +0200

    dokončený celok o komponentoch a bindingoch

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, //nazov classy z app componentu
+    ProductListComponent
   ],
   imports: [
     BrowserModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //prida App component ako startovaci
 })
 export class AppModule { }
