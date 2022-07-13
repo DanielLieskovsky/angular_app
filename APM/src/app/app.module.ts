ce2a95fd2925c67bddbf7b7f7715db1b915ffaa1
Daniel Lieskovsky <dano.lieskovsky@gmail.com>
2022-07-13 21:03:07 +0200

    pred routingom

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { WelcomeComponent } from './home/welcome.component';
+import { StarComponent } from './shared/star.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces-pipe';
+import { HttpClientModule} from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component'
 
 @NgModule({
   declarations: [
     AppComponent, //nazov classy z app componentu
-    ProductListComponent
+    WelcomeComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  bootstrap: [AppComponent] //prida App component ako startovaci
+  bootstrap: [AppComponent], //prida App component ako startovaci
 })
-export class AppModule { }
+export class AppModule {}
--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,31 +1,70 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, throwError } from 'rxjs';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
+  /*  styles: [
+    'thead {color: #ec6602;}',
+    '.tabNaZmenu {font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif}'
+  ] */
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Product list';
-  products: any[] = [
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2021',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.img',
-    },
-    {
-      productId: 5,
-      productName: 'Hammer',
-      productCode: 'TBX-0048',
-      releaseDate: 'May 21, 2021',
-      description: 'Curved claw steel hammer',
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: 'assets/images/hammer.img',
-    },
-  ];
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  filteredProducts: IProduct[] = [];
+  errorMessage: string = '';
+  sub!: Subscription;
+
+  private _listFilter: string = '';
+
+  constructor(private productService: ProductService) {};
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter: ', value);
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  products: IProduct[] = [];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+
+  }
+
+  ngOnDestroy(): void {
+      this.sub.unsubscribe();
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBy)
+    );
+  }
+
+  onRatingClicked(message: string) : void
+  {
+      this.pageTitle = 'Product List: ' + message;
+  }
 }
